f6a490db4ae315b976102270fd5e311227f044f9
patelritesh261 <patelritesh261@gmail.com>
2016-02-04 16:21:38 -0500

    gg

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res) {
+  res.render('service', { title: 'Services' });
+});
+
+/* GET services page. */
+router.get('/profilepage', function(req, res) {
+  res.render('profilepage', { title: 'Profile Page' });
+});
+
+/* GET services page. */
+router.get('/contactus', function(req, res) {
+  res.render('contactus', { title: 'Contact US' });
+});
+
 module.exports = router;
